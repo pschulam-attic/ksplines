9949794d46917ec3c43ec40b9071d3af9c48b734
Peter Schulam <pschulam@gmail.com>
2014-03-20 11:17:29 -0400

    Add plot method for ksfit objects.

--- R/plot.R
+++ R/plot.R
@@ -1,5 +1,5 @@
 #' @export
-plot.ksplines <- function(fit) {
+plot.ksplines <- function(fit, include_means = TRUE, print_immediately = TRUE) {
     curves <- fit$curves    
     curve_ids <- vapply(curves, "[[", integer(1), "id")
     curve_clusters <- data.frame(id = curve_ids, cluster = fit$cluster)
@@ -12,8 +12,24 @@ plot.ksplines <- function(fit) {
 
     data <- merge(data, curve_clusters, by = "id")
 
-    p <- ggplot(data) + theme_bw()
-    p <- p + geom_point(aes(x = x, y = y), alpha = 0.25)
+    means <- data.frame()
+    codebook <- fit$codebook
+    xlow <- min(data$x)
+    xhigh <- max(data$x)
+    x <- seq(xlow, xhigh, length = 100)
+    X <- fit$basis_func(x)
+
+    for (cx in unique(data$cluster)) {
+        mean_curve <- data.frame(cluster = cx,
+                                 x = x,
+                                 y = X %*% codebook[, cx])
+        means <- rbind(means, mean_curve)
+    }
+
+    p <- ggplot() + theme_bw()
+    p <- p + geom_point(aes(x = x, y = y), alpha = 0.25, data = data)
+    p <- p + geom_line(aes(x = x, y = y), size = 1.5, color = "firebrick1", data = means)
     p <- p + facet_wrap(~ cluster)
-    p
+    if (print_immediately) print(p)
+    invisible(p)
 }
